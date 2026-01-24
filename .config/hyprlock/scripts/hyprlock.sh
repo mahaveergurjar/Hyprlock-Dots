24a97fb131c65641de7d22c5271e60e5d33a3768
Mahaveer Gurjar <mahaveergurjar.work@gmail.com>
2026-01-24 21:31:58 +0530

    Enhance hyprlock script for Spotify and Glava
    Refactor hyprlock script to improve Spotify integration and manage Glava window.

--- .config/hyprlock/scripts/hyprlock.sh
+++ .config/hyprlock/scripts/hyprlock.sh
@@ -1,9 +1,23 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-if [[ "$(playerctl -p spotify status)" = "Playing"  ]]; then
+if [[ "$(playerctl -p spotify status 2>/dev/null)" == "Playing" ]]; then
+    pkill glava
+
+    # Start Glava (NOT desktop mode)
+    glava &
+    sleep 0.6
+
+    # Focus Glava window
+    hyprctl dispatch focuswindow class:glava
+    sleep 0.1
+
+    # Fullscreen focused window
+    hyprctl dispatch fullscreen 
+
+    # Lock screen
     hyprlock --config ~/.config/hyprlock/music.conf
+else
+    hyprlock
+fi
 
-else :
-    hyprlock 
-fi 
-
+pkill glava
